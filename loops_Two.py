869c523c1139c665976d2a72e9a913c70451ee8d
avi3165 <avi316532068@gmail.com>
2025-06-19 16:07:00 +0300

    add q2

--- loops_Two.py
+++ loops_Two.py
@@ -1,10 +1,19 @@
 #q1
-for c in range(1,1001):
-    if c%15==0:
-        print("FuzzBuzz")
-    elif c%5==0:
-        print("Buzz")
-    elif c%3==0:
-        print("Fuzz")
-    else:
-        print(c)
+# for c in range(1,1001):
+#     if c%15==0:
+#         print("FuzzBuzz")
+#     elif c%5==0:
+#         print("Buzz")
+#     elif c%3==0:
+#         print("Fuzz")
+#     else:
+#         print(c)
+#q2
+a=0
+b=int(input("Please enter a number: "))
+while a<b:
+    a += 1
+    print("*"*a)
+while a>0:
+    a-=1
+    print("*"*a)
