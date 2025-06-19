fe74db5d5a56bae6fc65f38104c57afe8dbce2aa
avi3165 <avi316532068@gmail.com>
2025-06-19 16:08:26 +0300

    add q3

--- loops_Two.py
+++ loops_Two.py
@@ -9,11 +9,19 @@
 #     else:
 #         print(c)
 #q2
-a=0
-b=int(input("Please enter a number: "))
-while a<b:
-    a += 1
-    print("*"*a)
-while a>0:
-    a-=1
-    print("*"*a)
+# a=0
+# b=int(input("Please enter a number: "))
+# while a<b:
+#     a += 1
+#     print("*"*a)
+# while a>0:
+#     a-=1
+#     print("*"*a)
+#q3
+a=int(input("Please enter a number:"))
+for i in range(2,a):
+    if a%i==0:
+        print("It is not a first number")
+        break
+    elif i==a-1:
+        print("It is a first number")
